811c76c818f0ec41d8c82ec08e4852727bca5aed
Gabriel Santos <gabrielsantana@magnasistemas.com.br>
2021-01-09 17:20:58 -0300

    Metodo POST implementado

--- backend/src/main/java/com/devsuperior/dsdelivey/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdelivey/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdelivey.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdelivey.dto.OrderDTO;
 import com.devsuperior.dsdelivey.services.OrderService;
@@ -15,15 +19,19 @@ import com.devsuperior.dsdelivey.services.OrderService;
 @RequestMapping(value = "/order")
 public class OrderController {
 
-	
 	@Autowired
 	private OrderService service;
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<OrderDTO>> findAll(){
-	List<OrderDTO> list = service.findAll(); //Usando Metodo já criado antes de retorno
-	return ResponseEntity.ok().body(list); //Retornando Metodo HTTP
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> list = service.findAll(); // Usando Metodo já criado antes de retorno
+		return ResponseEntity.ok().body(list); // Retornando Metodo HTTP
 
 	}
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
+}
--- backend/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdelivey.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdelivey.dto.OrderDTO;
+import com.devsuperior.dsdelivey.dto.ProductDTO;
 import com.devsuperior.dsdelivey.entities.Order;
+import com.devsuperior.dsdelivey.entities.OrderStatus;
+import com.devsuperior.dsdelivey.entities.Product;
 import com.devsuperior.dsdelivey.repositories.OrderRepository;
+import com.devsuperior.dsdelivey.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@Transactional(readOnly =  true)
 	public List<OrderDTO> findAll(){
@@ -25,6 +33,24 @@ public class OrderService {
 		//usando Lambda par receber a stream             Desfazendo a stream(VERBOSO)    
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		
+	}
+	
+	@Transactional          //Recebdo Pedido com os Produtos
+	public OrderDTO insert(OrderDTO dto){
+		//Inserindo Produto
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),				
+				Instant.now(), OrderStatus.PENDENTE);
+		
+		//Associando Produtos no Pedido
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		//Guardando referencia para o OBJ Order
+		order = orderRepository.save(order);	
+		
+	return new OrderDTO(order);
 	}
 	
 }
