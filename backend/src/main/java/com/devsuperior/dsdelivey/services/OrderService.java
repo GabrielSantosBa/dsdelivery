c989844818e7ef783207cf585b4fd19e257b6969
Gabriel Santos <gabrielsantana@magnasistemas.com.br>
2021-01-09 17:33:16 -0300

    Metodo PUT implementado

--- backend/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
@@ -21,36 +21,45 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
-	@Transactional(readOnly =  true)
-	public List<OrderDTO> findAll(){
-		
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+
 		List<Order> list = orderRepository.buscandoPedidosComProdutos();
-		
-		//usando Lambda par receber a stream             Desfazendo a stream(VERBOSO)    
+
+		// usando Lambda par receber a stream Desfazendo a stream(VERBOSO)
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-		
+
 	}
-	
-	@Transactional          //Recebdo Pedido com os Produtos
-	public OrderDTO insert(OrderDTO dto){
-		//Inserindo Produto
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),				
-				Instant.now(), OrderStatus.PENDENTE);
-		
-		//Associando Produtos no Pedido
-		for(ProductDTO p : dto.getProducts()) {
+
+	@Transactional // Recebdo Pedido com os Produtos
+	public OrderDTO insert(OrderDTO dto) {
+		// Inserindo Produto
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDENTE);
+
+		// Associando Produtos no Pedido
+		for (ProductDTO p : dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
-		
-		//Guardando referencia para o OBJ Order
-		order = orderRepository.save(order);	
-		
-	return new OrderDTO(order);
+
+		// Guardando referencia para o OBJ Order
+		order = orderRepository.save(order);
+
+		return new OrderDTO(order);
 	}
-	
+
+	@Transactional //Metodo PUT
+	public OrderDTO setDelivery(Long id) {
+		Order order = orderRepository.getOne(id);
+
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+
 }
